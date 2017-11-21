ba45b1a475ff6ddd710dfc20f7f2ca9b53542b8d
Andrii Mishkovskyi <amishkovskyi@schubergphilis.com>
2017-11-21 12:21:11 +0100

    Kicking off work on chef origin

---
+++ packages/@datapull/chef-origin/index.js
@@ -0,0 +1,17 @@
+{
+  "name": "@datapull/chef-origin",
+  "version": "0.0.1",
+  "description": "Data origin for Datapull: pull data from Chef API",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Andrii Mishkovskyi <amishkovskyi@schubergphilis.com>",
+  "license": "MIT",
+  "publishConfig": {
+    "access": "public"
+  },
+    "dependencies": {
+        "ursa": "^0.9.4"
+  }
+}
