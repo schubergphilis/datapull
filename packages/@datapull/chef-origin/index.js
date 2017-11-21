c100eb72a79e81a13cf7f5335fb8ed6df3114f8b
Andrii Mishkovskyi <amishkovskyi@schubergphilis.com>
2017-11-21 14:50:24 +0100

    Corrects the code commited for index.js

--- packages/@datapull/chef-origin/index.js
+++ packages/@datapull/chef-origin/index.js
@@ -1,17 +1,43 @@
-{
-  "name": "@datapull/chef-origin",
-  "version": "0.0.1",
-  "description": "Data origin for Datapull: pull data from Chef API",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "author": "Andrii Mishkovskyi <amishkovskyi@schubergphilis.com>",
-  "license": "MIT",
-  "publishConfig": {
-    "access": "public"
-  },
-    "dependencies": {
-        "ursa": "^0.9.4"
-  }
+const util = require('util');
+const request = require('request-promise-native');
+const chef_api = require("chef-api");
+
+class ChefOrigin {
+
+    constructor(config) {
+        this.config = config;
+    }
+
+    get originDeclaration() {
+        return {
+            name: ['Chef', this.config.organization].join(':'),
+            runner: this.pullData.bind(this)
+        };
+    }
+
+    async pullData(config) {
+        console.log('[Chef] pulling data for ', config.searchName);
+
+        const options = {
+            user_name: config.username,
+            key_path: config.key_path,
+            organization: this.config.organization
+        };
+
+        const chef = new chef_api();
+        chef.config(options);
+        nodes = utils.promisify(chef.getNodes)()
+        console.log(nodes);
+
+        return results;
+    }
 }
+
+async function sleep(msec) {
+    return new Promise(resolve => setTimeout(resolve, msec));
+}
+
+exports.datapullStep = {
+    isOrigin: true,
+    constructor: ChefOrigin
+};
