393a0d17bfe52ca628145775060f1601ed0cbd4a
Andrii Mishkovskyi <amishkovskyi@schubergphilis.com>
2017-11-22 10:00:37 +0100

    Addresses PR review comments

--- packages/@datapull/chef-origin/index.js
+++ packages/@datapull/chef-origin/index.js
@@ -16,7 +16,7 @@ class ChefOrigin {
     }
 
     async pullData(config) {
-        console.log('[Chef] pulling data for ', config.searchName);
+        console.log('[Chef] pulling data for ', this.config.organization);
 
         const options = {
             user_name: config.username,
@@ -30,47 +30,12 @@ class ChefOrigin {
         const results = [];
         for (const nodeName in nodes) {
             if (nodes.hasOwnProperty(nodeName)) {
-                const node = await util.promisify(chef.getNode)(nodeName);
-                const info = {
-                    name: nodeName,
-                    chef_environment: node['chef_environment'],
-                    automatic: {
-                        cpu: {
-                            total: objectPath.get(node, 'automatic.cpu.total'),
-                            cores: objectPath.get(node, 'automatic.cpu.cores'),
-                            real: objectPath.get(node, 'automatic.cpu.real')
-                        },
-                        kernel: {
-                            name: objectPath.get(node, 'automatic.kernel.name'),
-                            os: objectPath.get(node, 'automatic.kernel.os')
-                        },
-                        filesystem: objectPath.get(node, 'automatic.filesystem'),
-                        memory: {
-                            total: objectPath.get(node, 'automatic.memory.total'),
-                            free: objectPath.get(node, 'automatic.memory.free')
-                        },
-                        ec2: {
-                            instance_id: objectPath.get(node, 'automatic.ec2.instance_id'),
-                            instance_type: objectPath.get(node, 'automatic.ec2.instance_type')
-                        },
-                        os: objectPath.get(node, 'automatic.os'),
-                        os_version: objectPath.get(node, 'automatic.os_version'),
-                        platform: objectPath.get(node, 'automatic.platform'),
-                        uptime_seconds: objectPath.get(node, 'automatic.uptime_seconds'),
-                        last_contact: new Date(1000 * objectPath.get(node, 'automatic.ohai_time', 0)),
-                        hostname: objectPath.get(node, 'automatic.hostname'),
-                        packages: objectPath.get(node, 'automatic.packages'),
-                        cookbooks: objectPath.get(node, 'automatic.cookbooks')
-                    },
-                    run_list: node['run_list']
-
-                };
-                console.log(info);
-                results.push(info);
+                const node = util.promisify(chef.getNode)(nodeName);
+                results.push(node);
             }
         }
 
-        return results;
+        return await Promise.all(results);
     }
 }
 
