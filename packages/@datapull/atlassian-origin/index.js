90077da709a2639e26841680511fa4c17d0aa9b3
Andrii Mishkovskyi <amishkovskyi@schubergphilis.com>
2019-02-19 16:32:27 +0100

    Working version of Jira users collection

--- packages/@datapull/atlassian-origin/index.js
+++ packages/@datapull/atlassian-origin/index.js
@@ -1,58 +1,63 @@
 const processConfig = require('@datapull/json-config').processConfig;
 const got = require('got');
-const url = require('url');
-const http = require('https');
-const qs = require('querystring');
 
 class HttpApiOrigin {
-    constructor(stepConfig) {
-        // replace variables in the config:
-        try {
-            this.config = processConfig(stepConfig);
-        } catch (err) {
-            throw Error(`Could not parse config of Atlassian origin ${err}`);
-        }
+  constructor(stepConfig) {
+    const config = processConfig(stepConfig);
+    this.organisation = config.organisation;
+    this.username = config.username;
+    this.password = config.password;
+    this.batchSize = new Number(config.batchSize || 1000);
+    if (this.batchSize > 1000) {
+      throw new Error(`Atlassian API does not support returning more than 1000 users per call, you requested ${this.batchSize}`);
     }
+  }
 
-    get originDeclaration() {
-        return {
-            name: ['Atlassian', this.method, this.url].join(':'),
-            runner: this.pullData.bind(this)
-        };
+  get originDeclaration() {
+    return {
+      name: ['Atlassian', this.organisation].join(':'),
+      runner: this.pullData.bind(this)
+    };
+  }
+
+  async pullData(_) {
+    console.log(`[Atlassian Origin] pulling user data for ${this.organisation} with batch size ${this.batchSize}`);
+    let data = [];
+
+    // Since Atlassian does not have a proper API for listing all
+    // users in specific organisation, we are abusing "search" API to
+    // find all users and then paginate through them in an ad-hoc
+    // way. Note that said search API does not support pagination out
+    // of the box, so we simply iterate through results until hit an
+    // "incomplete" page.
+    let index = 0;
+
+    while (true) {
+      const options = {
+        protocol: 'https:',
+        method: 'get',
+        host: `${this.organisation}.atlassian.net`,
+        path: `/rest/api/latest/user/search?startAt=${index}&maxResults=${this.batchSize}&username=%`,
+        auth: `${this.username}:${this.password}`,
+        json: true
+      };
+      console.log(`[Atlassian Origin] HTTP GET ${options.host}/${options.path}`);
+      const response = await got(options);
+      data = data.concat(response.body);
+
+      // The definition of "incomplete page" is right here, number of
+      // results is smaller than requested batch size
+      if (response.body.length < this.batchSize) {
+        break
+      }
+      index += this.batchSize;
     }
-
-    pullData(pipelineConfig) {
-        console.log('[Atlassian Origin] fetching', this.method, this.url);
-
-        let mergedConfig = Object.assign(
-            {
-                method: 'get',
-                pageSize: 200,
-            },
-            pipelineConfig ? pipelineConfig.atlassian || {} : {},
-            this.config
-        );
-
-        // validate config:
-        if (!mergedConfig.url) {
-            throw Error('Atlassian Origin: url is not set up. Please specify `url` in the config');
-        }
-
-        if (!mergedConfig.username) {
-            throw Error('Atlassian Origin: username for basic auth is not set up');
-        }
-        if (!mergedConfig.password) {
-            throw Error('Atlassian Origin: password for basic auth is not set up');
-        }
-        const auth = `${mergedConfig.username}:${mergedConfig.password}`;
-
-
-
-    }
+    return data;
+  }
 
 }
 
 exports.datapullStep = {
-    isOrigin: true,
-    constructor: HttpApiOrigin
+  isOrigin: true,
+  constructor: HttpApiOrigin
 };
