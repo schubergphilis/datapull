a9a88d8296f83fc6b10423e6edf990d27a777866
Irina Krstic <ikrstic@schubergphilis.com>
2019-03-28 14:20:40 +0100

    atlassian origin changed

--- packages/@datapull/atlassian-origin/index.js
+++ packages/@datapull/atlassian-origin/index.js
@@ -7,9 +7,10 @@ class AtlassianOrigin {
     this.organisation = config.organisation;
     this.username = config.username;
     this.password = config.password;
-    this.batchSize = Number(config.batchSize || 1000);
+    this.batchSize = Number(config.batchSize || 50);
     this.service = config.service;
-    if (this.service === 'jira' && this.batchSize > 1000) {
+    this.includeInactiveUsers = config.includeInactiveUsers || false;
+    if (this.service === 'jira' && this.batchSize > 50) {
       throw new Error(`Atlassian API does not support returning more than 1000 users per call, you requested ${this.batchSize}`);
     }
   }
@@ -21,95 +22,46 @@ class AtlassianOrigin {
     };
   }
 
+  // To list all Atlassian users in specific organisation,
+  // that have access to jira tool,
+  // we have to find all users that are members of 'jira-software-users'.
+  // For those who have an access to confluence,
+  // we have to retrieve members of 'confluence-users' group.
   async pullData(_) {
     if (this.service === 'jira') {
       console.log(`[Atlassian Origin] pulling ${this.service} user data for ${this.organisation} with batch size ${this.batchSize}`);
-      return await this.getJiraData();
+      return await this.getUsers('jira-software-users');
     }
 
     if (this.service === 'confluence') {
       console.log(`[Atlassian Origin] pulling ${this.service} user data for ${this.organisation} with batch size ${this.batchSize}`);
-      return await this.getConfluenceData();
+      return await this.getUsers('confluence-users');
+
     }
     throw Error(`Atlassian Origin] ${this.service} is not supported. It must be jira or confluence`)
   }
 
-  async getJiraData() {
+  async getUsers(groupName) {
     let data = [];
 
-    // Since Atlassian does not have a proper API for listing all
-    // users in specific organisation, we are abusing "search" API to
-    // find all users and then paginate through them in an ad-hoc
-    // way. Note that said search API does not support pagination out
-    // of the box, so we simply iterate through results until hit an
-    // "incomplete" page.
     let index = 0;
+    let membersResponse = await this.getRequest(groupName, index);
+    data = membersResponse.body['values'];
 
-    while (true) {
-      const path = `/rest/api/latest/user/search?startAt=${index}&maxResults=${this.batchSize}&username=%`;
-      const response = await this.getRequest(path);
-      data = data.concat(response.body);
-
-      // The definition of "incomplete page" is right here, number of
-      // results is smaller than requested batch size
-      if (response.body.length < this.batchSize) {
-        break
-      }
+    while (!membersResponse.body['isLast']) {
       index += this.batchSize;
+      membersResponse = await this.getRequest(groupName, index);
+      data = data.concat(membersResponse.body['values']);
     }
     return data;
   }
 
-  async getConfluenceData() {
-    let groups = [];
-    let users = new Map();
-
-    // lists all Confluence groups and finds members for each group
-
-    let groupsResponse = await this.getGroups(`/wiki/rest/api/group?start=0&limit=${this.batchSize}`, groups);
-
-    while ('next' in groupsResponse.body['_links']) {
-      groupsResponse = await this.getGroups(
-          groupsResponse.body['_links']['context']  + groupsResponse.body['_links']['next'],
-          groups);
-    }
-
-    for (const groupName of groups) {
-      const path = `/wiki/rest/api/group/${groupName}/member?start=0&limit=${this.batchSize}`;
-
-      let membersResponse = await this.getMembers(path, users);
-      while ('next' in membersResponse.body['_links']) {
-        membersResponse = await this.getMembers(
-            membersResponse.body['_links']['context'] + membersResponse.body['_links']['next'],
-            users);
-      }
-    }
-
-    return Array.from(users.values());
-  }
-
-  async getMembers(path, users) {
-    let membersResponse = await this.getRequest(path);
-    for (const user of membersResponse.body['results']) {
-      users.set(user['accountId'], user);
-    }
-    return membersResponse;
-  }
-
-  async getGroups(path, groups) {
-    let groupsResponse = await this.getRequest(path);
-    for (const result of groupsResponse.body['results']) {
-      groups.push(result['name']);
-    }
-    return groupsResponse;
-  }
-
-  async getRequest(path) {
+  async getRequest(groupName, index) {
     const options = {
       protocol: 'https:',
       method: 'get',
       host: `${this.organisation}.atlassian.net`,
-      path: path,
+      path:  `/rest/api/3/group/member?groupname=${groupName}&includeInactiveUsers=${this.includeInactiveUsers}&startAt=${index}&maxResults=${this.batchSize}`,
       auth: `${this.username}:${this.password}`,
       json: true
     };
