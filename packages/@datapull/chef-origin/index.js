97b9458a5621f97645922d4203ee1500061f5eb7
Irina Krstic <ikrstic@schubergphilis.com>
2019-04-16 10:37:13 +0200

    chef origin - refactored code

--- packages/@datapull/chef-origin/index.js
+++ packages/@datapull/chef-origin/index.js
@@ -16,29 +16,27 @@ class ChefOrigin {
 
     async pullData(pipelineConfig) {
         console.log('[Chef] pulling data for ', pipelineConfig.apiUrl);
-        let options;
-        if (pipelineConfig.key) {
-            options = {
-                user_name: pipelineConfig.username,
-                key: pipelineConfig.key,
-                url: pipelineConfig.apiUrl
-            };
-        } else {
-            if (!pipelineConfig.key_path) {
-                console.error(`[Chef origin] key path or key must be specified`);
-                throw Error(`Key path or key must be specified`);
-            }
+        const options = {
+            user_name: pipelineConfig.username,
+            url: pipelineConfig.apiUrl
+        };
 
+        if (!pipelineConfig.key && !pipelineConfig.key_path) {
+            console.error(`[Chef origin] key path or key must be specified`);
+            throw Error(`Key path or key must be specified`);
+        }
+
+        if (pipelineConfig.key) {
+            options.key = pipelineConfig.key;
+        }
+
+        if(pipelineConfig.key_path) {
             if (!fs.existsSync(pipelineConfig.key_path)) {
                 console.error(`[Chef origin] key does not exist in the given path`);
                 throw Error(`Key does not exist in the given path`);
             }
 
-            options = {
-                user_name: pipelineConfig.username,
-                key_path: pipelineConfig.key_path,
-                url: pipelineConfig.apiUrl
-            };
+            options.key_path = pipelineConfig.key_path;
         }
 
         const chef = new chef_api();
